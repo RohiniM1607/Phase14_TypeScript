076457c7f54bcf4ce9e3b07990229b6301cbdb53
RohiniM1607 <mrohini7558@gmail.com>
2026-06-23 08:30:36 +0530

    Input file changed

--- Handson/Sequence/E2_AreaOfCircle.ts
+++ Handson/Sequence/E2_AreaOfCircle.ts
@@ -1,6 +1,5 @@
 let radius: number = Number(process.argv[2]);
 
 let area: number = 3.14 * radius * radius;
-
 console.log("Radius = " + radius);
 console.log("Area = " + area);
--- Handson/Sequence/E3_SumOfTwoNumbers.ts
+++ Handson/Sequence/E3_SumOfTwoNumbers.ts
@@ -1,7 +1,11 @@
-import promptSync from "prompt-sync";
-const prompt = promptSync();
+import * as fs from "fs";
 
-let num1: number = Number(prompt("Enter the first number: "));
-let num2: number = Number(prompt("Enter the second number: "));
+const lines = fs.readFileSync("Input.txt", "utf8")
+                .split(/\r?\n/);
+
+let num1: number = Number(lines[1]); // 2nd line
+let num2: number = Number(lines[2]); // 3rd line
+
 let sum = num1 + num2;
-console.log("The sum of "+num1+" and "+num2+" is "+sum);
+
+console.log("The sum of " + num1 + " and " + num2 + " is " + sum);
